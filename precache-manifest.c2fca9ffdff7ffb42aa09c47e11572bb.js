fe036898944d5705f1a87280461947ff58e72f06
bobanko <mishko.vladimir@gmail.com>
2020-01-29 10:57:21 +0200

    Updates

---
+++ precache-manifest.c2fca9ffdff7ffb42aa09c47e11572bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5750e3c0d29457fdf17f4ecf87bf2acf",
+    "url": "/pg-web/index.html"
+  },
+  {
+    "revision": "22b807de288d9e8165f2",
+    "url": "/pg-web/static/css/main.0683290d.chunk.css"
+  },
+  {
+    "revision": "fe4ca9dafe2b7b3e7225",
+    "url": "/pg-web/static/js/2.574192ac.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/pg-web/static/js/2.574192ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22b807de288d9e8165f2",
+    "url": "/pg-web/static/js/main.ce7268b7.chunk.js"
+  },
+  {
+    "revision": "17da32c55a812f2b138d",
+    "url": "/pg-web/static/js/runtime-main.677f96d2.js"
+  },
+  {
+    "revision": "b5f18203baf0ebb385875d95bfe52182",
+    "url": "/pg-web/static/media/pg-background.b5f18203.png"
+  },
+  {
+    "revision": "e035a70d357436c4ea337f9e32685fd2",
+    "url": "/pg-web/static/media/pg-icon.e035a70d.svg"
+  }
+]);
