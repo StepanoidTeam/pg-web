d64d42567194735f40b40d4e6de885b6a5f20c60
Volodymyr Myshko <volodymyrmyshko@Administrators-MacBook-Pro-2.local>
2020-01-29 10:30:48 +0200

    build #1 for github

---
+++ build/precache-manifest.53cd305a7029073b9e71c77956e37fe1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e7ad501de01c77104ae04cd6ce10cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "795a4389294a71f121dd",
+    "url": "/static/css/main.768550a7.chunk.css"
+  },
+  {
+    "revision": "fe4ca9dafe2b7b3e7225",
+    "url": "/static/js/2.574192ac.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/static/js/2.574192ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "795a4389294a71f121dd",
+    "url": "/static/js/main.18c4b3aa.chunk.js"
+  },
+  {
+    "revision": "c87f383deae389a1045e",
+    "url": "/static/js/runtime-main.bd978735.js"
+  },
+  {
+    "revision": "b5f18203baf0ebb385875d95bfe52182",
+    "url": "/static/media/pg-background.b5f18203.png"
+  },
+  {
+    "revision": "e035a70d357436c4ea337f9e32685fd2",
+    "url": "/static/media/pg-icon.e035a70d.svg"
+  }
+]);
