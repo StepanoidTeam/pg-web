e3086b13643e638664a973da8ff7b55d90b0a189
bobanko <mishko.vladimir@gmail.com>
2020-02-02 05:44:16 +0200

    input: restyling

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -8,27 +8,38 @@ export default class Input extends React.Component {
     super(props);
 
     this.state = {
-      value: ""
+      isFocused: false
     };
   }
 
   render() {
     const { label, value, helperText, onChange } = this.props;
+    const { isFocused } = this.state;
+    const isEmpty = value.length === 0;
 
     return (
-      <label className="mb-2 cursor-pointer">
-        <div className="input">
-          <div className="text-block">
+      <label
+        className={cx("input mb-2 cursor-pointer", {
+          "is-focused": isFocused,
+          "is-empty": isEmpty
+        })}
+      >
+        <div className="input-block">
+          <div className="text-block flex-column">
             <div className="label-text">{label}</div>
 
             <input
               className="input-value w-100"
               type="text"
               value={value}
+              placeholder=" "
               onChange={event => onChange(event.target.value)}
+              onFocus={() => this.setState({ isFocused: true })}
+              onBlur={() => this.setState({ isFocused: false })}
             />
           </div>
         </div>
+        <hr className="m-0" />
         <div className="input-helper flex-row">
           <span>{helperText}</span>
         </div>
