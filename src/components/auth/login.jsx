e2717e7f0092ca3d78d9bfbd5ae84ebd2b2e120f
bobanko <mishko.vladimir@gmail.com>
2020-02-02 22:04:27 +0200

    login: styles

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -31,8 +31,11 @@ export default class Login extends React.Component {
           onChange={password => this.setState({ password })}
         />
 
-        <button className="button">log in</button>
-        <span>
+        <button className="button flex-row align-center justify-center">
+          <span>log in</span>
+          <i className="material-icons">arrow_forward</i>
+        </button>
+        <span className="p-2 flex-row justify-between">
           Don't have an account?
           <a href="/register">register</a>
         </span>
