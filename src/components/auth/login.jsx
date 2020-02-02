2ee8a7809100e5ef3d8b54ffee9051a782d34551
bobanko <mishko.vladimir@gmail.com>
2020-02-03 01:28:39 +0200

    login: migrate to hooks

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,63 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../common/input";
 import { logIn } from "../../services/auth.service";
+import { useHistory } from "react-router-dom";
 
-export default class Login extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Login() {
+  const [username, setUsername] = useState("kekster2000");
+  const [password, setPassword] = useState("qwerty123");
+  const history = useHistory();
 
-    this.state = {
-      username: "kekster2000",
-      password: "qwerty123"
-    };
-  }
-
-  logIn = () => {
-    const { username, password } = this.state;
-
+  const onLogIn = () => {
     logIn({ username, password })
       .then(data => {
         console.log("login ok", data);
 
-        this.props.history.push("/rooms");
+        history.push("/rooms");
       })
       .catch(error => {
         console.warn("login failed", error);
       });
   };
 
-  render() {
-    const { username, password } = this.state;
+  return (
+    <div className="form flex-column">
+      <Input
+        label="username"
+        value={username}
+        helperText="username should be strong enough"
+        icon="face"
+        onChange={value => setUsername(value)}
+      />
+      <Input
+        label="password"
+        value={password}
+        helperText="keep password simple"
+        icon="lock"
+        onChange={value => setPassword(value)}
+      />
 
-    return (
-      <div className="form flex-column">
-        <Input
-          label="username"
-          value={username}
-          helperText="username should be strong enough"
-          icon="face"
-          onChange={username => this.setState({ username })}
-        />
-        <Input
-          label="password"
-          value={password}
-          helperText="keep password simple"
-          icon="lock"
-          onChange={password => this.setState({ password })}
-        />
-
-        <button
-          className="button flex-row align-center justify-center"
-          onClick={this.logIn}
-        >
-          <span>log in</span>
-          <i className="material-icons">arrow_forward</i>
-        </button>
-        <span className="p-2 flex-row justify-between">
-          Don't have an account?
-          <a href="/register">register</a>
-        </span>
-      </div>
-    );
-  }
+      <button
+        className="button flex-row align-center justify-center"
+        onClick={onLogIn}
+      >
+        <span>log in</span>
+        <i className="material-icons">arrow_forward</i>
+      </button>
+      <span className="p-2 flex-row justify-between">
+        Don't have an account?
+        <a href="/register">register</a>
+      </span>
+    </div>
+  );
 }
