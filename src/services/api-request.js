673a84f5b78916f0fd8a7c26a2a1bfbd5ccc3e43
bobanko <mishko.vladimir@gmail.com>
2020-02-04 01:22:08 +0200

    api-request: json error fix

--- src/services/api-request.js
+++ src/services/api-request.js
@@ -3,12 +3,15 @@ import { apiUrl } from "../config";
 export async function apiRequest(endpoint, ...rest) {
   try {
     const response = await fetch(`${apiUrl}/${endpoint}`, ...rest);
-    if (!response.ok) throw response.statusText;
+
+    if (!response.ok) {
+      throw await response.json();
+    }
 
     const json = await response.json();
     return json.data;
   } catch (error) {
-    console.warn("error", error);
+    console.warn("api error", error);
 
     return Promise.reject(error);
   }
