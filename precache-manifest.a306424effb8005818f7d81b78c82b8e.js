602db721f348c87b292d0d35e11159b99a4ea94c
bobanko <mishko.vladimir@gmail.com>
2020-02-05 03:54:52 +0200

    Updates

---
+++ precache-manifest.a306424effb8005818f7d81b78c82b8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cf7fefe7f92d6c121a69f145203799",
+    "url": "/pg-web/index.html"
+  },
+  {
+    "revision": "c06019d36ae88cdbc7c4",
+    "url": "/pg-web/static/css/main.8534fc71.chunk.css"
+  },
+  {
+    "revision": "d3829c6ab7e8845a7dcf",
+    "url": "/pg-web/static/js/2.ef2fa344.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/pg-web/static/js/2.ef2fa344.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c06019d36ae88cdbc7c4",
+    "url": "/pg-web/static/js/main.fec9e8d4.chunk.js"
+  },
+  {
+    "revision": "17da32c55a812f2b138d",
+    "url": "/pg-web/static/js/runtime-main.677f96d2.js"
+  },
+  {
+    "revision": "b5f18203baf0ebb385875d95bfe52182",
+    "url": "/pg-web/static/media/pg-background.b5f18203.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pg-web/precache-manifest.a1b7dea72730ccae06e9afc12d780e25.js"
+  "/pg-web/precache-manifest.a306424effb8005818f7d81b78c82b8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pg-web/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
