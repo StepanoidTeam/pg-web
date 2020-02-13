29a16eacde82bbb35f08b455154f7d1102a39f10
bobanko <mishko.vladimir@gmail.com>
2020-02-13 23:18:36 +0200

    version: singletone removed

--- src/services/version.service.js
+++ src/services/version.service.js
@@ -1,13 +1,5 @@
 import { apiRequest } from "./api-request";
 
-//helper
-//const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-//api method
-export async function getVersion() {
-  //await sleep(3000);
+export async function getApiVersion() {
   return apiRequest("version").then(data => data.version);
 }
-
-//singletone?
-export const $apiVersion = getVersion();
