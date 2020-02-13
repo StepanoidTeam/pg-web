43269859459fd9e3462f82e76f102aaad22e8d29
bobanko <mishko.vladimir@gmail.com>
2020-02-13 23:21:38 +0200

    version-holder: refac to function

--- src/components/version-holder.js
+++ src/components/version-holder.js
@@ -1,45 +1,25 @@
-import React from "react";
-import { $apiVersion } from "../services/version.service";
+import React, { useEffect } from "react";
 
+import { useGlobal } from "../use-global";
+import { getApiVersion } from "../services/version.service";
 import "./version-holder.css";
 
 const { version: webVersion } = require("../../package.json");
-export default class VersionHolder extends React.Component {
-  constructor(props) {
-    super(props);
 
-    $apiVersion
-      .then(apiVersion => {
-        console.log(apiVersion);
-        this.setState({ apiVersion });
-      })
-      .catch(err => {
-        console.log("err", err);
-        this.setState({ hasError: err });
-      })
-      .finally(() => {
-        this.setState({ isLoading: false });
-      });
+export default function VersionHolder() {
+  const [{ apiVersion }, { setApiVersion, setOnline }] = useGlobal();
 
-    this.state = {
-      isLoading: true,
-      hasError: null,
-      apiVersion: null,
-      webVersion
-    };
-  }
+  useEffect(() => {
+    getApiVersion().then(apiVersion => {
+      setApiVersion(apiVersion);
+      setOnline(true);
+    });
+  }, []);
 
-  render() {
-    const { apiVersion, hasError, isLoading } = this.state;
-
-    return (
-      <div className="version-holder flex-column m-1">
-        {<span>web: {webVersion}</span>}
-        {isLoading && <span>loading...</span>}
-        {!isLoading && apiVersion && <span>api: {apiVersion}</span>}
-
-        {hasError ? <span>Has error: {JSON.stringify(hasError)}</span> : null}
-      </div>
-    );
-  }
+  return (
+    <div className="version-holder flex-column m-1">
+      <span>web: {webVersion}</span>
+      <span>api: {apiVersion}</span>
+    </div>
+  );
 }
