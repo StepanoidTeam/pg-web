eded37a8e1c08a6ed5261589a679a0de0ef17fa5
bobanko <mishko.vladimir@gmail.com>
2020-02-16 12:49:54 +0200

    room: kickPlayer, createRoom impl basic

--- src/services/room.service.js
+++ src/services/room.service.js
@@ -1,37 +1,64 @@
-import { apiRequest } from "./api-request";
+import { apiRequest } from './api-request';
 
 export function getRoomList(authToken) {
-  return apiRequest("room/list", {
+  return apiRequest('room/list', {
     // todo(vmyshko): make common
     headers: {
-      "Content-type": "application/json",
-      authToken
+      'Content-type': 'application/json',
+      authToken,
     },
-    method: "GET"
+    method: 'GET',
     //body: JSON.stringify({ username, password })
   });
 }
 
 export function joinRoom(authToken, roomId) {
-  return apiRequest("room/join", {
+  return apiRequest('room/join', {
     // todo(vmyshko): make common
     headers: {
-      "Content-type": "application/json",
-      authToken
+      'Content-type': 'application/json',
+      authToken,
     },
-    method: "POST",
-    body: JSON.stringify({ roomId })
+    method: 'POST',
+    body: JSON.stringify({ roomId }),
   });
 }
 
 export function leaveRoom(authToken) {
-  return apiRequest("room/leave", {
+  return apiRequest('room/leave', {
     // todo(vmyshko): make common
     headers: {
-      "Content-type": "application/json",
-      authToken
+      'Content-type': 'application/json',
+      authToken,
     },
-    method: "POST"
+    method: 'POST',
     //body: JSON.stringify({ roomId })
+  });
+}
+
+export function kickPlayer(authToken, playerId) {
+  return apiRequest('room/kick', {
+    // todo(vmyshko): make common
+    headers: {
+      'Content-type': 'application/json',
+      authToken,
+    },
+    method: 'POST',
+    body: JSON.stringify({ username: playerId }),
+  });
+}
+export function createRoom(authToken, roomConfig) {
+  return apiRequest('room/create', {
+    // todo(vmyshko): make common
+    headers: {
+      'Content-type': 'application/json',
+      authToken,
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      name: 'new room',
+      // setReadyMark: true,
+      // gameRounds: 0,
+    }),
   });
 }
