f1386bb19e74d52e1ba405d029a17224290f1dab
bobanko <mishko.vladimir@gmail.com>
2020-02-17 03:35:16 +0200

    nearestConnection: calc module impl

---
+++ src/components/map/city-connector-offsets.js
@@ -0,0 +1,49 @@
+import { minBy } from 'lodash';
+import { getDistance } from './math-helpers';
+
+//init
+const cityBlockSize = { width: 190, height: 150 }; //80
+const cols = 3;
+const rows = 2;
+
+const cityConnectorOffsets = [];
+const slotSize = {
+  width: cityBlockSize.width / cols,
+  height: cityBlockSize.height / rows,
+};
+
+for (let col = 0; col < cols; col++) {
+  for (let row = 0; row < rows; row++) {
+    cityConnectorOffsets.push({
+      x: slotSize.width * (col - (cols - 1) / 2),
+      y: slotSize.height * (row - (rows - 1) / 2),
+    });
+  }
+}
+
+function nearestConnection(from, to) {
+  const fromOffsets = cityConnectorOffsets.map(cco => ({
+    x: from.x + cco.x,
+    y: from.y + cco.y,
+  }));
+
+  const toOffsets = cityConnectorOffsets.map(cco => ({
+    x: to.x + cco.x,
+    y: to.y + cco.y,
+  }));
+
+  //get all distances between
+  const dists = [];
+
+  fromOffsets.forEach(fromPoint => {
+    toOffsets.forEach(toPoint => {
+      dists.push([fromPoint, toPoint, getDistance(fromPoint, toPoint)]);
+    });
+  });
+
+  const [nearestFrom, nearestTo, dist] = minBy(dists, ([, , dist]) => dist);
+
+  return [nearestFrom, nearestTo, dist];
+}
+
+export { nearestConnection };
