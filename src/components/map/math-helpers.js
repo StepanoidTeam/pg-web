b99c7e5c45731f5b652680b0cd7afa0e5ab868ab
bobanko <mishko.vladimir@gmail.com>
2020-02-17 05:03:27 +0200

    math: getMIddlePoint impl

--- src/components/map/math-helpers.js
+++ src/components/map/math-helpers.js
@@ -1,3 +1,17 @@
+/**
+ * returns distance between two points
+ * @param {{x:number, y:number}} from
+ * @param {{x:number, y:number}} to
+ */
 export function getDistance(from, to) {
   return ((from.x - to.x) ** 2 + (from.y - to.y) ** 2) ** 0.5;
+}
+
+/**
+ * returns point between two other points
+ * @param {{x:number, y:number}} from
+ * @param {{x:number, y:number}} to
+ */
+export function getMiddlePoint(from, to) {
+  return { x: from.x + (to.x - from.x) / 2, y: from.y + (to.y - from.y) / 2 };
 }
