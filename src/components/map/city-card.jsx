dd0df164d6af82dde9a2dcb21b190e906f60ae5d
bobanko <mishko.vladimir@gmail.com>
2020-02-17 03:37:14 +0200

    minor restyling

--- src/components/map/city-card.jsx
+++ src/components/map/city-card.jsx
@@ -11,30 +11,38 @@ export default function CityCard(props) {
   const { x, y, name } = props;
 
   return (
-    <div
-      style={{
-        left: x,
-        top: y,
-      }}
-      className="city-card"
-    >
-      <div className="city-rows flex-column p-1">
-        <div className="city-slots flex-row">
-          {[10, 15, 20].map(cost => (
-            <CitySlot key={cost} cost={cost} />
-          ))}
-        </div>
-        <div className="position-relative">
-          <div className="overlay flex-row align-center justify-center">
-            <div className="city-name px-1 text-stroke">{name}</div>
+    <>
+      <div
+        className="overlay flex-row justify-center align-center"
+        style={{
+          left: x,
+          top: y,
+        }}
+      >
+        <div className="city-card">
+          <div className="city-rows flex-column p-1">
+            <div className="city-slots flex-row">
+              {[10, 15, 20].map(cost => (
+                <CitySlot key={cost} cost={cost} />
+              ))}
+            </div>
+            <div className="position-relative">
+              <div className="overlay w-100 h-100 flex-row align-center justify-center">
+                <div className="city-name px-1 text-stroke">{name}</div>
+              </div>
+            </div>
+            <div
+              className="city-slots flex-row"
+              // hidden
+              //hidden={Math.random() * 10 > 5}
+            >
+              {[10, 15, 20].map(cost => (
+                <CitySlot key={cost} cost={cost} />
+              ))}
+            </div>
           </div>
         </div>
-        <div className="city-slots flex-row" hidden={Math.random() * 10 > 5}>
-          {[10, 15, 20].map(cost => (
-            <CitySlot key={cost} cost={cost} />
-          ))}
-        </div>
       </div>
-    </div>
+    </>
   );
 }
