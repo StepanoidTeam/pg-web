13e2e00f2257e7bc3509fd2f0b119030355e8030
bobanko <mishko.vladimir@gmail.com>
2020-02-28 03:18:48 +0200

    status-holder: cleanup

--- src/components/auth/status-holder.jsx
+++ src/components/auth/status-holder.jsx
@@ -1,32 +1,12 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 
-import { getStatus } from '../../services/auth.service';
+import { logOut } from '../../services/auth.service';
 import { useGlobal } from '../../use-global';
 
 import './status-holder.css';
 
 export default function StatusHolder() {
-  const history = useHistory();
-  const [
-    { authToken, isAuthenticated, user },
-    { setUserData, clearUserData },
-  ] = useGlobal();
-
-  useEffect(() => {
-    authToken &&
-      !isAuthenticated &&
-      getStatus(authToken)
-        .then(data => {
-          setUserData(data);
-
-          //if user in room - redirect
-          if (data.GameRoomId) {
-            history.push(`rooms/${encodeURIComponent(data.GameRoomId)}`);
-          }
-        })
-        .catch(clearUserData);
-  }, []);
+  const [{ isAuthenticated, user }, {}] = useGlobal();
 
   return (
     <div className="status-holder flex-column align-end p-1 z-index-1">
@@ -39,7 +19,7 @@ export default function StatusHolder() {
 
           <button
             className="button flex-row align-center px-3"
-            onClick={clearUserData}
+            onClick={logOut}
           >
             <span> log out</span>
             <i className="material-icons">exit_to_app</i>
