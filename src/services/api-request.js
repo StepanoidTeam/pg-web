fbc61486924af9081fb959a8a502028dfa6affc0
bobanko <mishko.vladimir@gmail.com>
2020-03-03 03:24:52 +0200

    api-request: refac, IS_LOCAL_ENV, dbDoc, dbCollection impl

--- src/services/api-request.js
+++ src/services/api-request.js
@@ -1,8 +1,60 @@
-import { apiUrl } from "../config";
+import * as firebase from 'firebase/app';
 
-export async function apiRequest(endpoint, ...rest) {
+import { apiUrl } from '../config';
+import { firebaseConfig } from '../config';
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+const IS_LOCAL_ENV = ['192.168.', 'localhost'].some(h =>
+  window.location.hostname.startsWith(h),
+);
+
+if (IS_LOCAL_ENV) {
+  console.log('🏠local env detected!');
+  db.settings({
+    host: `${window.location.hostname}:8080`,
+    ssl: false,
+  });
+} else {
+  console.log('🌐global env used');
+}
+
+export function dbDoc(entityName, id) {
+  const entityRef = db.collection(entityName).doc(id);
+  const snapFn = callback => doc => callback({ id: doc.id, ...doc.data() });
+
+  return {
+    get: callback => entityRef.get().then(snapFn(callback)),
+    onUpdate: callback => entityRef.onSnapshot(snapFn(callback)),
+  };
+}
+
+export function dbCollection(entityName) {
+  const entityRef = db.collection(entityName);
+  const snapFn = callback => snap =>
+    callback(snap.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+
+  return {
+    get: callback => entityRef.get().then(snapFn(callback)),
+    onUpdate: callback => entityRef.onSnapshot(snapFn(callback)),
+  };
+}
+
+export async function apiRequest(endpoint, options = {}) {
+  const { currentUser } = await firebase.auth();
+
+  const authToken = currentUser ? await currentUser.getIdToken(true) : '';
+
   try {
-    const response = await fetch(`${apiUrl}/${endpoint}`, ...rest);
+    const response = await fetch(`${apiUrl}/${endpoint}`, {
+      headers: {
+        'Content-type': 'application/json',
+        authorization: `Bearer ${authToken}`,
+      },
+      ...options,
+    });
 
     if (!response.ok) {
       throw await response.json();
@@ -11,7 +63,7 @@ export async function apiRequest(endpoint, ...rest) {
     const json = await response.json();
     return json.data;
   } catch (error) {
-    console.warn("api error", error);
+    console.warn('api error', error);
 
     return Promise.reject(error);
   }
