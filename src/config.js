ab5578474685ba0b32cdc6da9e18b27cdd7fa772
bobanko <mishko.vladimir@gmail.com>
2020-03-03 03:30:34 +0200

    config: cloud functions + local

--- src/config.js
+++ src/config.js
@@ -1,8 +1,14 @@
-export const apiUrl = 'https://pg-api.azurewebsites.net/api';
 export const wsUrl = 'wss://pg-api.azurewebsites.net/api';
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyBrLjfuTRzphTHh_9V7Aik-jTLylWfAnt0',
   authDomain: 'power-grid-c1320.firebaseapp.com',
   projectId: 'power-grid-c1320',
+
+  functions: 'https://us-central1-power-grid-c1320.cloudfunctions.net/',
+  functionsLocal: `http://localhost:5001/power-grid-c1320/us-central1/`,
 };
+// todo(vmyshko): impl dynamic url
+//${window.location.origin}
+// export const apiUrl = 'https://pg-api.azurewebsites.net/api';
+export const apiUrl = firebaseConfig.functionsLocal + 'api';
