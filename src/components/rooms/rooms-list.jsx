de63d0c8e7ad87a85c11202626c66fb392afee3e
bobanko <mishko.vladimir@gmail.com>
2020-03-03 03:33:28 +0200

    rooms-list: cleanup, fb basic impl

--- src/components/rooms/rooms-list.jsx
+++ src/components/rooms/rooms-list.jsx
@@ -1,57 +1,53 @@
 import React, { useEffect } from 'react';
-import * as firebase from 'firebase/app';
 
-import { useGlobal } from '../../use-global';
-import { getRoomList, joinRoom } from '../../services/room.service';
+import { roomList, joinRoom } from '../../services/room.service';
 import { useHistory } from 'react-router-dom';
 
 import './rooms.css';
+import { useState } from 'react';
 
 export default function RoomList() {
   const history = useHistory();
-  const [{ authToken, rooms }, { setRooms }] = useGlobal();
+  const [rooms, setRooms] = useState([]);
 
   const onRoomCreate = () => {
     history.push(`/rooms/new`);
   };
 
   const onRoomJoin = roomId => {
-    joinRoom(authToken, roomId).then(data => {
-      // todo(vmyshko): get rid of # in game object ids (izya)
-      history.push(`/rooms/${encodeURIComponent(roomId)}`);
+    joinRoom(roomId).then(data => {
+      history.push(`/rooms/${roomId}`);
     });
   };
 
   useEffect(() => {
-    return getRoomList().onUpdate(rooms => setRooms(rooms));
-
-    // getRoomList().get(rooms => console.log('🔥', rooms));
+    return roomList().onUpdate(rooms => {
+      console.log(rooms);
+      setRooms(rooms);
+    });
   }, []);
 
   return (
     <div className="rooms form flex-column p-2">
       <h1 className="mx-2">rooms</h1>
+
       <ul className="room-list list flex-column p-1 m-2">
-        {rooms
-          //[...rooms, ...rooms, ...rooms]
-          //.filter(r => !r.IsInGame)
-          .map(room => (
-            <li
-              className="list-item flex-row align-center p-2 m-1"
-              key={room.id}
-            >
-              <span className="room-name fill-left">{room.Name}</span>
+        {rooms.length === 0 && <li>no open rooms, but you can create one</li>}
 
-              <div className="flex-row align-center">
-                <i className="material-icons">group</i>
-                <span className="px-1">{room.UserCount}</span>
+        {rooms.map(room => (
+          <li className="list-item flex-row align-center p-2 m-1" key={room.id}>
+            <span className="room-name fill-left">{room.name}</span>
 
-                <button className="button" onClick={() => onRoomJoin(room.Id)}>
-                  join
-                </button>
-              </div>
-            </li>
-          ))}
+            <div className="flex-row align-center">
+              <i className="material-icons">group</i>
+              <span className="px-1">{room.playerCount}</span>
+
+              <button className="button" onClick={() => onRoomJoin(room.id)}>
+                join
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
 
       <button
