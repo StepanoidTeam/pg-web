ccc50c509c1db1e5bc9dca2af6c32c2fd01658ae
bobanko <mishko.vladimir@gmail.com>
2020-03-03 03:31:55 +0200

    api: game - headers cleanup

--- src/services/game.service.js
+++ src/services/game.service.js
@@ -1,37 +1,21 @@
-import { apiRequest } from "./api-request";
+import { apiRequest } from './api-request';
 
-export function getGameStatus(authToken) {
-  return apiRequest("game/status", {
-    // todo(vmyshko): make common
-    headers: {
-      "Content-type": "application/json",
-      authToken
-    },
-    method: "GET"
-    //body: JSON.stringify({ roomId })
+export function getGameStatus() {
+  return apiRequest('game/status', {
+    method: 'GET',
   });
 }
 
-export function changeColor(authToken, userId) {
-  return apiRequest("game/changeColor", {
-    // todo(vmyshko): make common
-    headers: {
-      "Content-type": "application/json",
-      authToken
-    },
-    method: "POST",
-    body: JSON.stringify({ userId })
+export function changeColor(userId) {
+  return apiRequest('game/changeColor', {
+    method: 'POST',
+    body: JSON.stringify({ userId }),
   });
 }
 
-export function toggleReady(authToken, state = true) {
-  return apiRequest("game/toggleReady", {
-    // todo(vmyshko): make common
-    headers: {
-      "Content-type": "application/json",
-      authToken
-    },
-    method: "POST",
-    body: JSON.stringify({ state })
+export function toggleReady(state = true) {
+  return apiRequest('game/toggleReady', {
+    method: 'POST',
+    body: JSON.stringify({ state }),
   });
 }
